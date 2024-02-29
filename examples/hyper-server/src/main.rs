772a6c556ceddaa0a8eb88d7a27c2dd266bba518
Leevi Koponen <contact@leevikoponen.fi>
2024-02-29 10:06:10 +0200

    Make the server run on multiple threads
    Works in a primarily shared nothing approach with each worker
    acting entirely independently by utilizing SO_REUSEPORT

--- examples/hyper-server/src/main.rs
+++ examples/hyper-server/src/main.rs
@@ -1,48 +1,57 @@
-use std::{
-    io::Result,
-    net::{SocketAddr, TcpListener},
-    os::fd::AsFd,
-    rc::Rc,
-};
+use std::{io::Result, net::SocketAddr, num::NonZeroUsize, os::fd::AsFd, rc::Rc};
 
 use clap::Parser;
 use hyper::{header::CONTENT_TYPE, server::conn::http1::Builder, Response, StatusCode};
 use hyper_util::rt::TokioIo;
 use io_uring::IoUring;
 use local_fifo_executor::Executor;
+use socket2::{Protocol, SockAddr, Socket, Type};
 use uring_adapter::PollIo;
 use uring_reactor::Reactor;
 
 #[derive(Debug, Parser)]
 struct Arguments {
+    #[arg(long, env, default_value = "2")]
+    workers: NonZeroUsize,
     #[arg(long, env, default_value = "256")]
     entries: u32,
+    #[arg(long, env, default_value = "512")]
+    backlog: i32,
     #[arg(long, env, default_value = "[::]:8080")]
     address: SocketAddr,
 }
 
-fn main() -> Result<()> {
-    let arguments = Arguments::parse();
+fn start(arguments: &Arguments, index: usize) -> Result<()> {
     let executor = Rc::new(Executor::new());
     let reactor = IoUring::new(arguments.entries)
         .map(Reactor::new)
         .map(Rc::new)?;
 
-    let listener = TcpListener::bind(arguments.address)?;
-    listener.set_nonblocking(true)?;
+    let address = SockAddr::from(arguments.address);
+    let socket = Socket::new(
+        address.domain(),
+        Type::STREAM.nonblocking(),
+        Some(Protocol::TCP),
+    )?;
 
-    let address = listener.local_addr()?;
-    println!("listening on {address}");
+    socket.set_nonblocking(true)?;
+    socket.set_reuse_address(true)?;
+    socket.set_reuse_port(true)?;
+    socket.bind(&address)?;
+    socket.listen(arguments.backlog)?;
+
+    let actual = socket.local_addr()?.as_socket().unwrap();
+    println!("worker {index} listening on {actual}");
 
     let task = executor.spawn(async {
         loop {
-            let (stream, address) = uring_operation::accept(&reactor, listener.as_fd()).await?;
+            let (stream, address) = uring_operation::accept(&reactor, socket.as_fd()).await?;
 
             let address = address.as_socket().unwrap();
             let connection = Builder::new().serve_connection(
                 TokioIo::new(PollIo::new(reactor.clone(), stream)),
                 hyper::service::service_fn(move |_| async move {
-                    let message = format!("Hello, there {address}!\n",);
+                    let message = format!("Hello to you {address} from worker {index}\n");
 
                     Response::builder()
                         .status(StatusCode::OK)
@@ -65,4 +74,19 @@ fn main() -> Result<()> {
         executor.tick();
         reactor.tick()
     })?
+}
+
+fn main() -> Result<()> {
+    let arguments = Arguments::parse();
+
+    std::thread::scope(|scope| {
+        (0..arguments.workers.get())
+            .map(|index| {
+                let arguments = &arguments;
+                scope.spawn(move || start(arguments, index))
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
+            .try_for_each(|thread| thread.join().unwrap())
+    })
 }
